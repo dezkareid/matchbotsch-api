04dfd2ca11edfaf47bb3b858371b083a5ae5c37f
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-25 00:35:57 -0500

    added parts endpoint to api

--- api/index.js
+++ api/index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const { api: meta } = require('../config')
 const cors = require('cors')
 const api = express.Router()
+const partsRouter = require('./parts')
 
 api.use(cors())
+api.use(express.json())
 
 api.all('/', (req, res) => {
   res.json(meta)
 })
+
+api.use('/parts', partsRouter)
 
 module.exports = api
