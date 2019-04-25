0cbb7a137a62c4ebd0c6ccbb6e11d20a05343821
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-25 01:16:55 -0500

    added partName

--- api/parts/search.js
+++ api/parts/search.js
@@ -2,33 +2,43 @@ const fakeParts = {
   parts: [
     {
       partNumber: '253626',
+      partName: 'Bujia',
       retailer: 'Don Pepe',
       image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250'
+      price: '250',
+      details: ''
     },
     {
       partNumber: '253626',
+      partName: 'Bujia',
       retailer: 'Don Pepe',
       image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250'
+      price: '250',
+      details: ''
     },
     {
       partNumber: '253626',
+      partName: 'Bujia',
       retailer: 'Don Pepe',
       image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250'
+      price: '250',
+      details: ''
     },
     {
       partNumber: '253626',
+      partName: 'Bujia',
       retailer: 'Don Pepe',
       image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250'
+      price: '250',
+      details: ''
     },
     {
       partNumber: '253626',
+      partName: 'Bujia',
       retailer: 'Don Pepe',
       image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250'
+      price: '250',
+      details: ''
     }
   ]
 }
