b43edac3e974de3e2d6b327ae180b0c852b09354
DezkaReid <elmaildeldezkareid@gmail.com>
2019-04-25 09:28:53 -0500

    functionality to search

--- api/parts/search.js
+++ api/parts/search.js
@@ -1,50 +1,12 @@
-const fakeParts = {
-  parts: [
-    {
-      partNumber: '253626',
-      partName: 'Bujia',
-      retailer: 'Don Pepe',
-      image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250',
-      details: 'http://www.homerswebpage.com/'
-    },
-    {
-      partNumber: '253626',
-      partName: 'Bujia',
-      retailer: 'Don Pepe',
-      image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250',
-      details: 'http://www.homerswebpage.com/'
-    },
-    {
-      partNumber: '253626',
-      partName: 'Bujia',
-      retailer: 'Don Pepe',
-      image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250',
-      details: 'http://www.homerswebpage.com/'
-    },
-    {
-      partNumber: '253626',
-      partName: 'Bujia',
-      retailer: 'Don Pepe',
-      image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250',
-      details: 'http://www.homerswebpage.com/'
-    },
-    {
-      partNumber: '253626',
-      partName: 'Bujia',
-      retailer: 'Don Pepe',
-      image: 'https://refaccionariamario.info/26791-tm_large_default/bujia-bosch-fr7ldc.jpg',
-      price: '250',
-      details: 'http://www.homerswebpage.com/'
-    }
-  ]
-}
+const partsModule = require('../../modules/parts')
+const authModule = require('../../modules/authentication')
 
 function search (req, res) {
-  res.json(fakeParts)
+  const { year, make, model, submodel, part } = req.body
+  authModule.requestToken()
+    .then(() => partsModule.search(year, make, model, submodel, part))
+    .then(parts => res.json({ parts: parts.slice(0, 4) }))
+    .catch(console.error)
 }
 
 module.exports = search
